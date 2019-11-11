0b4235222cde6056aa2515ad287af92270c3901a
Jaemin Jung <windyvill@gmail.com>
2019-11-11 18:05:18 +0900

    프로젝트 시작

---
+++ Assets/Scripts/MainButtonManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Handle))]
+
+public class MainButtonManager : MonoBehaviour
+{
+    [SerializeField] Button SubButton;
+
+    public enum Handle {   HandleType1,HandleType2 };
+     
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+
+
+}
