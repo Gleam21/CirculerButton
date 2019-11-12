d2f13f7d3f59d9d7827ed12f64337f1a276fcd91
Jaemin Jung <windyvill@gmail.com>
2019-11-12 15:24:39 +0900

    마우스 드래그 직선 궤적

--- Assets/Scripts/ActionButtonManager.cs
+++ Assets/Scripts/ActionButtonManager.cs
@@ -1,42 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class ActionButtonManager : MonoBehaviour
 {
     [SerializeField] GameObject MainButtonPrefab;
     [SerializeField] GameObject SubButtonPrefab;
 
+    Vector2 strPos;
+    Vector2 endPos;
 
-
-
-
-    public void EndDrag()
-    {
-
-        Debug.Log("드래그 끝");
-    }
-
-
-    public void OnMouseDrag()
-    {
-      //  Debug.Log(Input.mousePosition);
-         
-    }
-
+    public Image image;
 
     // Start is called before the first frame update
     void Start()
     {
         
-    }
 
-    // Update is called once per frame
+    }
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            image.gameObject.SetActive(true);
+            strPos = Input.mousePosition;
+            image.transform.position = strPos;
+          
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            Vector2 myPos = Input.mousePosition;
+            image.transform.localScale = new Vector2(Vector2.Distance(myPos, strPos), 1);
+            image.transform.localRotation = Quaternion.Euler(0, 0,
+                AngleInDeg(strPos, myPos));
+           
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            image.gameObject.SetActive(false);
+       
+        }
     }
 
+    public static float AngleInRad(Vector3 vec1, Vector3 vec2)
+    {
+        return Mathf.Atan2(vec2.y - vec1.y, vec2.x - vec1.x);
+    }
+
+    public static float AngleInDeg(Vector3 vec1, Vector3 vec2)
+    {
+        return AngleInRad(vec1, vec2) * 180 / Mathf.PI;
+    } 
 
 
 
